312ed975cc0157f0e65a536d987301eb79e78c85
mouazourassen18 <54610297+mouazourassen18@users.noreply.github.com>
2021-09-07 21:36:13 -0400

    firstPart_FinalProject

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
-function CreateArea() {
+function CreateArea(props) {
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input
+          onChange={props.handleChange}
+          name="title"
+          placeholder="Title :"
+          value={props.title}
+        />
+        <textarea
+          onChange={props.handleChange}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={props.content}
+        />
         <button>Add</button>
       </form>
     </div>
